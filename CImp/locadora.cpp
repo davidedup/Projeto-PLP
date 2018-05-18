57044c8f3842afef7f9218f1e70fbd5c551c952c
marcusvlc <marcus.costa@ccc.ufcg.edu.br>
2018-05-18 14:44:02 -0300

    Adicionando funcionalidade na area de gerente, cadastrar filme feito

--- CImp/locadora.cpp
+++ CImp/locadora.cpp
@@ -2,20 +2,89 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <vector>
+using std::vector;
+using std::string;
 using namespace std;
 
-class Filme {
+struct Filme {
     string nome;
     int ano;
     string genero;
     string descricao;
 };
 
-class Usuario {
+
+struct Usuario {
+    string cpf;
     string nome;
 
+
 };
+
+void apresentacao();
+void editarFilme();
+void opcoesGerente();
+
+vector<Filme> filmesCadastrados;
+
+void cadastrarFilme(){
+    Filme filme;
+
+    try {
+        cout<<"Insira o nome do filme: ";
+        cin>>filme.nome;
+        cout<<"Insira o ano do filme: ";
+        cin>>filme.ano;
+        cout<<"Insira o genero do filme: ";
+        cin>>filme.genero;
+        cout<<"Insira uma descriçao para o filme: ";
+        cin>>filme.descricao;
+
+        filmesCadastrados.push_back(filme);
+
+        int op;
+        cout<<"Deseja cadastrar outro filme?"<<endl;
+        cout<<"(1) SIM"<<endl;
+        cout<<"(2) NÃO"<<endl;
+
+        cin>>op;
+
+        if(op == 1 ) {
+            cadastrarFilme();
+        } else if(op == 2) {
+            opcoesGerente();
+        }
+
+    } catch(exception e) {
+        cout<<"Erro no cadastro de filme"<<endl;
+    }
+
+}
+
+void opcoesGerente() {
+
+        int opcao;
+        cout<<"Informe uma opçao para seguir:"<<endl;
+        cout<<"(1) CADASTRAR FILME NO SISTEMA"<<endl;
+        cout<<"(2) EDITAR FILME DO SISTEMA"<<endl;
+        cout<<"(3) VOLTAR PARA O MENU PRINCIPAL"<<endl;
+
+        cin>>opcao;
+
+        if(opcao == 1) {
+            cadastrarFilme();
+        } else if (opcao == 2) {
+            opcoesGerente();
+        } else if(opcao == 3) {
+            apresentacao();
+        } else {
+             cout<<"Opcao invalida"<<endl;
+             opcoesGerente();
+        }
+
+}
+
 void gerente(){
     string codigo;
     cout<<"Bem-vindo a sessão de gerência do sistema"<<endl;
@@ -23,12 +92,19 @@ void gerente(){
     cin>>codigo;
 
     if(codigo == "admin") {
-        cout<<"Informe uma opçao para seguir:"<<endl;
+        opcoesGerente();
+
+    } else {
+        cout<<"Código incorreto!"<<endl;
+        gerente();
     }
+}
+
+
+void editarFilme(){}
 
 
 
-};
 void selecionaUsuario(string nome){};
 
 void apresentacao() {
@@ -50,14 +126,21 @@ void apresentacao() {
 
     cin>>opcao;
 
-    if(opcao == 1) gerente();
-    else {
+    if(opcao == 1) {
+        gerente();
+    }
+    else if(opcao == 2){
         string nome;
         cout<<"QUAL O SEU NOME DE USUÁRIO?"<<endl;
         cin>>nome;
         selecionaUsuario(nome);
+    } else {
+        cout<<"SELECIONE UMA OPÇAO VALIDA!"<<endl;
+        apresentacao();
     }
 }
+
+
 
 int main(){
 
