5c499bfc2ed8a3bf82b7320c85a7c18a42f24ae6
docinho <livia.juliao@ccc.ufcg.edu.br>
2018-06-20 20:39:21 -0300

    reorganizando variaveis

--- HaskellImp/locadora.hs
+++ HaskellImp/locadora.hs
@@ -1,11 +1,18 @@
-contadorDeFilmes = 0
-usuariosCadastrados = [(009, "Dummy")]
-sugestoes = []
-filmesCadastrados = [(1, ["Vingadores: Guerra do Infinito", "Fantasia", "2018", "Homem de Ferro, Thor, Hulk e os Vingadores se unem para combater seu inimigo mais poderoso, o maligno Thanos. Em uma missão para coletar todas as seis pedras infinitas, Thanos planeja usá-las para infligir sua vontade maléfica sobre a realidade."]), (2,["Black Panther", "Fantasia", "2018", "Conheça a história de T'Challa, príncipe do reino de Wakanda, que perde o seu pai e viaja para os Estados Unidos, onde tem contato com os Vingadores. Entre as suas habilidades estão a velocidade, inteligência e os sentidos apurados."]), (3, ["Deadpool 2", "Fantasia", "2018", "Sequência das aventuras do Mercenário Tagarela, interpretado por Ryan Reynolds. Na história original, o herói adquire superpoderes após uma experiência científica, e decide se vingar da pessoa responsável por sequestrar sua namorada."]), (4,["Oito Mulheres e um Segredo", "Thriller", "2018", "Recém-saída da prisão, Debbie Ocean planeja executar o assalto do século em pleno Met Gala, em Nova York, com o apoio de Lou, Nine Ball, Amita, Constance, Rose, Daphne Kluger e Tammy."])]
+data Filme = Filme {
+	nome :: String,
+	genero :: String,
+	ano :: String,
+	descricao :: String
+} deriving (Show)
+
 main:: IO ()
 main = do
+	let contadorDeFilmes = 0
+	let sugestoes = []
+	let filmesCadastrados = [Filme { nome = "Vingadores: Guerra do Infinito", genero = "Fantasia", ano = "2018", descricao = "Homem de Ferro, Thor, Hulk e os Vingadores se unem para combater seu inimigo mais poderoso, o maligno Thanos. Em uma missão para coletar todas as seis pedras infinitas, Thanos planeja usá-las para infligir sua vontade maléfica sobre a realidade." }, Filme { nome = "Black Panther", genero = "Fantasia", ano = "2018", descricao = "Conheça a história de T'Challa, príncipe do reino de Wakanda, que perde o seu pai e viaja para os Estados Unidos, onde tem contato com os Vingadores. Entre as suas habilidades estão a velocidade, inteligência e os sentidos apurados." }, Filme { nome = "Deadpool 2", genero = "Fantasia", ano = "2018", descricao = "Sequência das aventuras do Mercenário Tagarela, interpretado por Ryan Reynolds. Na história original, o herói adquire superpoderes após uma experiência científica, e decide se vingar da pessoa responsável por sequestrar sua namorada." }, Filme { nome = "Oito Mulheres e um Segredo", genero = "Thriller", ano = "2018", descricao = "Recém-saída da prisão, Debbie Ocean planeja executar o assalto do século em pleno Met Gala, em Nova York, com o apoio de Lou, Nine Ball, Amita, Constance, Rose, Daphne Kluger e Tammy." }]
 	menuPrint
 	menuOpcao
+	
 
 menuOpcao:: IO ()
 menuOpcao = do
