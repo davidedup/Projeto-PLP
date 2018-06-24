cac54de0d150885b5f721a0bdd218f7b742da8ff
marcusvlc <marcus.costa@ccc.ufcg.edu.br>
2018-06-24 09:48:51 -0300

    Adicionando a opcao de listar filmes por genero

--- HaskellImp/david.hs
+++ HaskellImp/david.hs
@@ -9,18 +9,19 @@ data Filme = Filme {
     disponivel :: Bool 
 } deriving (Show)
 
-filmesCadastrados = [Filme {indice = 1, nome = "Vingadores: Guerra do Infinito", genero = "Fantasia", ano = "2018", descricao = "Homem de Ferro, Thor, Hulk e os Vingadores se unem para combater seu inimigo mais poderoso, o maligno Thanos. Em uma missão para coletar todas as seis pedras infinitas, Thanos planeja usá-las para infligir sua vontade maléfica sobre a realidade.", disponivel = False },
-					 Filme {indice = 2, nome = "Black Panther", genero = "Fantasia", ano = "2018", descricao = "Conheça a história de T'Challa, príncipe do reino de Wakanda, que perde o seu pai e viaja para os Estados Unidos, onde tem contato com os Vingadores. Entre as suas habilidades estão a velocidade, inteligência e os sentidos apurados.", disponivel = True  },
+filmesCadastrados = [Filme {indice = 1, nome = "Vingadores: Guerra do Infinito", genero = "fantasia", ano = "2018", descricao = "Homem de Ferro, Thor, Hulk e os Vingadores se unem para combater seu inimigo mais poderoso, o maligno Thanos. Em uma missão para coletar todas as seis pedras infinitas, Thanos planeja usá-las para infligir sua vontade maléfica sobre a realidade.", disponivel = False },
+					 Filme {indice = 2, nome = "Black Panther", genero = "fantasia", ano = "2018", descricao = "Conheça a história de T'Challa, príncipe do reino de Wakanda, que perde o seu pai e viaja para os Estados Unidos, onde tem contato com os Vingadores. Entre as suas habilidades estão a velocidade, inteligência e os sentidos apurados.", disponivel = True  },
 					 Filme {indice = 3, nome = "Deadpool 2", genero = "Fantasia", ano = "2018", descricao = "Sequência das aventuras do Mercenário Tagarela, interpretado por Ryan Reynolds. Na história original, o herói adquire superpoderes após uma experiência científica, e decide se vingar da pessoa responsável por sequestrar sua namorada.", disponivel = True  },
-					 Filme {indice = 4, nome = "Oito Mulheres e um Segredo", genero = "Thriller", ano = "2018", descricao = "Recém-saída da prisão, Debbie Ocean planeja executar o assalto do século em pleno Met Gala, em Nova York, com o apoio de Lou, Nine Ball, Amita, Constance, Rose, Daphne Kluger e Tammy.", disponivel = True  }]
+					 Filme {indice = 4, nome = "Oito Mulheres e um Segredo", genero = "thriller", ano = "2018", descricao = "Recém-saída da prisão, Debbie Ocean planeja executar o assalto do século em pleno Met Gala, em Nova York, com o apoio de Lou, Nine Ball, Amita, Constance, Rose, Daphne Kluger e Tammy.", disponivel = True  }]
 
-meusAlugueis = [Filme {indice = 1, nome = "Vingadores: Guerra do Infinito", genero = "Fantasia", ano = "2018", descricao = "Homem de Ferro, Thor, Hulk e os Vingadores se unem para combater seu inimigo mais poderoso, o maligno Thanos. Em uma missão para coletar todas as seis pedras infinitas, Thanos planeja usá-las para infligir sua vontade maléfica sobre a realidade.", disponivel = False }]
+meusAlugueis = [Filme {indice = 1, nome = "Vingadores: Guerra do Infinito", genero = "fantasia", ano = "2018", descricao = "Homem de Ferro, Thor, Hulk e os Vingadores se unem para combater seu inimigo mais poderoso, o maligno Thanos. Em uma missão para coletar todas as seis pedras infinitas, Thanos planeja usá-las para infligir sua vontade maléfica sobre a realidade.", disponivel = False }]
 
 main :: IO ()
 main = do
     menuPrint
     menuOpcao
-	
+
+
 toStringFilme :: Filme -> String  
 toStringFilme (Filme {indice = i, nome = n, genero = g, ano = a, descricao = d, disponivel = dis }) = show i ++ " - " ++ n ++ " - Disponivel: " ++ show dis    
     
@@ -50,20 +51,30 @@ listarFilmesDisponiveis :: [Filme] -> String
 listarFilmesDisponiveis [] = ""
 listarFilmesDisponiveis (x:xs) = if estaDisponivel x then toStringFilme x ++ ['\n'] ++ listarFilmesDisponiveis xs else listarFilmesDisponiveis xs
 
+ehDoGenero:: Filme -> String -> Bool
+ehDoGenero (Filme {indice = i, nome = n, genero = g, ano = a, descricao = d, disponivel = dis }) gen = if g == gen then True else False
+
+listarFilmesPorGenero:: [Filme] -> String -> String
+listarFilmesPorGenero [] gen = ""
+listarFilmesPorGenero (x:xs) gen = if ehDoGenero x gen then toStringFilme x ++ ['\n'] ++ listarFilmesPorGenero xs gen else listarFilmesPorGenero xs gen
+
 opcaoEscolhida :: Int -> IO()
 opcaoEscolhida opcao | opcao == 1 = do {putStrLn (listarFilmes filmesCadastrados) ; menuOpcao} 
                      | opcao == 2 = do {putStrLn (listarFilmesDisponiveis filmesCadastrados) ; menuOpcao}
                      | opcao == 3 = do {putStrLn (listarFilmes meusAlugueis) ; menuOpcao}
-                     | opcao == 4 = do {realizaAluguel ; menuOpcao}    
+                     | opcao == 4 = do {realizaAluguel ; menuOpcao}
+                     | opcao == 5 = do {putStrLn "==> Insira o nome do genero na qual você deseja filtrar" ; genero <- getLine ; putStrLn "==> Os filmes que possuem esse genero são:" ;  putStrLn (listarFilmesPorGenero ( filmesCadastrados ) ( genero ) ) ; menuOpcao}    
                      | otherwise =  do {putStrLn "Opcao invalida, Porfavor escolha uma opcao valida" ; menuOpcao}
+
 
 menuOpcao :: IO ()
 menuOpcao = do
-    putStrLn "0 - sair" 
-    putStrLn "1 - listar filmes"
-    putStrLn "2 - listar filmes disponiveis"
-    putStrLn "3 - listar os alugados"
+    putStrLn "0 - Sair" 
+    putStrLn "1 - Listar filmes"
+    putStrLn "2 - Listar filmes disponiveis"
+    putStrLn "3 - Listar os alugados"
     putStrLn "4 - Realizar Aluguel"
+    putStrLn "5 - Listar filmes por genero"
     opcao <- getLine
     if (read opcao) == 0 then putStrLn("Fim") else do opcaoEscolhida (read opcao)
     
